5979dffdb03711ea4772922fe235281d0f6a11ea
Jordan Plows <jordan@plows.ai>
2026-08-18 22:53:17 -0700

    Update LICENSE, deps, build config; add src/cli and test/cli directories

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,32 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  outExtension({ format }) {
-    return {
-      js: format === "esm" ? ".js" : ".cjs",
-    };
+export default defineConfig([
+  // Library build
+  {
+    entry: ["src/index.ts"],
+    format: ["esm", "cjs"],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    outDir: "dist",
+    outExtension({ format }) {
+      return {
+        js: format === "esm" ? ".js" : ".cjs",
+      };
+    },
   },
-});
+  // CLI build
+  {
+    entry: ["src/cli/index.ts"],
+    format: ["esm"],
+    dts: false,
+    splitting: false,
+    sourcemap: false,
+    outDir: "dist/cli",
+    shims: true,
+    banner: {
+      js: "#!/usr/bin/env node",
+    },
+  },
+]);
